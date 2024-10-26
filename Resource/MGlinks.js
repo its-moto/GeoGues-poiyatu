62493dec0da6a06f8760a970176f1af04d68db6e
it_s_moto <111728630+its-moto@users.noreply.github.com>
2024-10-26 19:53:21 +0900

    Add files via upload

---
+++ Resource/MGlinks.js
@@ -0,0 +1,44 @@
+
+
+const streetViews = [
+    {//アンタナナリボ
+        embedUrl: 'https://www.google.com/maps/embed?pb=!4v1729748056745!6m8!1m7!1s6za1FUktl2TfG4VvuQQymg!2m2!1d-18.92413604984545!2d47.5303773151325!3f353.42137757176124!4f0!5f0.7820865974627469',
+        answerUrl: 'https://maps.app.goo.gl/mq85emWsLafHGUgB6',
+        lat: -18.924136,
+        lng: 47.5303773
+    },{
+        embedUrl: 'https://www.google.com/maps/embed?pb=!4v1729748169063!6m8!1m7!1sw1IFREiC31x8UdNct80Ajg!2m2!1d-22.09643716388154!2d46.76869654909648!3f58.296507447950304!4f0!5f0.7820865974627469',
+        answerUrl: 'https://maps.app.goo.gl/rVVMPr38iQb4vsoh6',
+        lat: -22.0964372, 
+        lng: 46.7686965
+    },{//RN8
+        embedUrl: 'https://www.google.com/maps/embed?pb=!4v1729558515149!6m8!1m7!1sT3l8G1-u-whp-D_ON55Ahg!2m2!1d-20.24951093684258!2d44.41981496911148!3f33.69!4f1.730000000000004!5f0.7820865974627469',
+        answerUrl: 'https://maps.app.goo.gl/CDihGVEvPw7FqANb6',
+        lat: -20.2495109,
+        lng: 44.419815
+    },{
+        embedUrl: 'https://www.google.com/maps/embed?pb=!4v1729748280172!6m8!1m7!1sqphmHGtMyYIaivwXMslUnw!2m2!1d-23.35543941590885!2d43.66870509739654!3f217.47046392841867!4f0!5f0.7820865974627469',
+        answerUrl: 'https://maps.app.goo.gl/sbDbVz9FUCLNjZ77A',
+        lat: -23.3554394, 
+        lng: 43.6687051
+    },{
+        embedUrl: 'https://www.google.com/maps/embed?pb=!4v1729748408119!6m8!1m7!1sVg5fwWp5wLwMtCHhNAWn9A!2m2!1d-13.47998450787207!2d48.37172165082281!3f277.1460460528473!4f-5.565119182109456!5f0.7820865974627469',
+        answerUrl: 'https://maps.app.goo.gl/bfeN9cythqbN1GiDA',
+        lat: -13.4799845,
+        lng: 48.3717217
+
+    },{
+        embedUrl: 'https://www.google.com/maps/embed?pb=!4v1729748562605!6m8!1m7!1sask63xylYk6fD8PUJ1muQA!2m2!1d-13.67461862265171!2d48.45172214761642!3f359.7327229387385!4f-1.8056639865623794!5f0.7820865974627469',
+        answerUrl: 'https://maps.app.goo.gl/X6dqrR3krNra2FV37',
+        lat: -13.6746186,
+        lng: 48.4517221
+    }
+    /*
+    ,{
+        embedUrl: '',
+        answerUrl: '',
+        lat: ,
+        lng:
+    }
+    */
+];
---
+++ Resource/SMlinks.js
@@ -0,0 +1,12 @@
+const streetViews = [
+    {
+        embedUrl: 'https://www.google.com/maps/embed?pb=!4v1729644947787!6m8!1m7!1syzk8hKHKoH6_tblpLXgqnA!2m2!1d43.9768262380029!2d12.48695842099183!3f90!4f0!5f0.7820865974627469',
+        answerUrl: 'https://maps.app.goo.gl/LtcHWZfwqwsrbpZJA'
+    }
+    /*
+    ,{
+        embedUrl: '',
+        answerUrl: ''
+    }
+    */
+];
