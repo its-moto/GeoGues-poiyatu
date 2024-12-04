ba418bf6768378755059931c2e24298646d643e7
it_s_moto <111728630+its-moto@users.noreply.github.com>
2024-12-04 20:21:26 +0900

    Add files via upload

--- Resource/src/SingleGamePlay.js
+++ Resource/src/SingleGamePlay.js
@@ -5,12 +5,11 @@ let currentLat; //現在の〜
 let currentLng; //緯度経度
 let nowDisplay = 'play'; //spaceキーで進行するための変数
 let nowSteeing = 'close'; //設定開いてるかの判定
-let Size; //???????????
 let marker; // ピンを保持する変数
 let rmarker; // ピンを保持する変数
 let ranswermarker; // ピンを保持する変数
-let rline; //リザルトの点線v
-let noPin = true;
+let rline; //リザルトの点線
+let noPin = true; //ピンおいたかどうか(時間切れ用)
 
 const RPZbool =localStorage.getItem('RPZbool') === 'true';
 const NMPZbool =localStorage.getItem('NMPZbool') === 'true';
@@ -29,10 +28,17 @@ const bgmTracks = [
     "../bgm/Track05.mp3",
     "../bgm/Track06.mp3"
 ];
-const ambientBgm = "../bgm/Trackmbient.mp3";
+const ambientBgm = "../bgm/Ambient.mp3";
 
 
 function showRandomStreetView() {
+    
+    document.getElementById('loading').style.display = 'flex';
+    clearInterval(timerInterval); // タイマー停止
+    timerInterval = null;
+    let totalRound = document.getElementById('totalRound');
+    totalRound.textContent = parseFloat(totalRound.textContent)+1;
+    
     closeResult();
     if(NMPZbool){
         document.getElementById('NMPZ-overlay').style.display = 'flex';
@@ -49,11 +55,6 @@ function showRandomStreetView() {
 
     noPin = true;
     let embedUrl = streetViews[randomIndex].embedUrl;
-
-    if (totalTime != 0) {
-        countdownTime = totalTime;
-        timerInterval = setInterval(updateTimer, 50);
-    }
 
     currentLat = parseFloat((embedUrl.match(/1d([-+]?[0-9]*\.?[0-9]+)/))[1]);
     currentLng = parseFloat((embedUrl.match(/2d([-+]?[0-9]*\.?[0-9]+)/))[1]);
@@ -84,10 +85,37 @@ function backToStart() {
     document.getElementById('streetview').src = currentBackToViewIndex;
 }
 
+function showAnswer() {
+    // hensuu[3] のリンクを新しいタブで開く
+    const link = streetViews[currentStreetViewIndex].answerUrl;
+    if (link) {
+        window.open(link, "_blank");
+    } else {
+        alert("リンクが見つかりません！");
+    }
+};
+
+//
+// iframe要素を取得
+const iframe = document.getElementById("streetview");
+
+// 読み込み完了時のイベントリスナーを設定
+iframe.addEventListener("load", () => {
+    // iframeが完全に読み込まれた後に実行
+    setTimeout(() => {
+        document.getElementById('loading').style.display = 'none';
+        if (totalTime != 0 && timerInterval == null) {
+            countdownTime = totalTime;
+            timerInterval = setInterval(updateTimer, 50);
+        }
+    }, 1000); // 遅延時間（ミリ秒）
+});
+
 //=================================================タイマーとか==================================================
 
 let countdownTime; // 初期時間（秒単位）
-let timerInterval;
+let timerInterval = null;
+let timerLeftAlertOp = 0;
 const totalTime = localStorage.getItem('timer') || 120; // タイマーの総時間を保存
 
 if (totalTime == 0) {
@@ -100,6 +128,20 @@ function formatTime(seconds) {
     return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
 }
 
+function LowTimerAlert() {
+    const timerOLay = document.getElementById('timerLowLeft').style;
+    if (localStorage.getItem('timer') > 30 ){
+        if (countdownTime < 30 && countdownTime >29.5){
+            timerLeftAlertOp = 1;
+        } else if (countdownTime < 29.5 && timerLeftAlertOp > 0){
+            timerLeftAlertOp = countdownTime-28.5;
+        } else {
+            timerLeftAlertOp = 0;
+        }
+        timerOLay.opacity = timerLeftAlertOp;
+    }
+}
+
 function updateTimer() {
     const countdownElement = document.getElementById('countdown');
     const progressRing = document.querySelector('.timer');
@@ -109,11 +151,16 @@ function updateTimer() {
     // プログレス更新（CSS変数）
     const progress = ((totalTime - countdownTime) / totalTime) * 100; // 経過時間の割合（%）
     progressRing.style.setProperty('--progress', `${progress}%`);
-
     if (countdownTime > 0) {
         countdownTime -= 0.05;
+        LowTimerAlert();
+        if (countdownTime < 15){
+            progressRing.style.background = "conic-gradient(rgba(0,0,0,0) var(--progress ,0%) ,#E94560 0%)";
+        } else {
+            progressRing.style.background = "conic-gradient(rgba(0,0,0,0) var(--progress ,0%) ,#7950E5 0%)";
+        }
+        
     } else {
-        clearInterval(timerInterval); // タイマー停止
         guess(); // タイマー終了時の処理
     }
 }
@@ -126,18 +173,18 @@ function setMapStage() {
         map.setView([0,0], 1);
         rfmap.setView([20,0], 2);
         mapSize =1;
-    } else if (mapName === "日本") {
+    } else if (mapName === "日本" || mapName === "An Urban Japan" || mapName === "SDF") {
         map.setView([37, 135], 4);
         rfmap.setView([38,138], 5);
         mapSize =7;
-    } else if (mapName === "An Urban Japan") {
-        map.setView([37, 135], 4);
-        rfmap.setView([38,138], 5);
-        mapSize = 7;
     } else if (mapName === "マダガスカル") {
         map.setView([-19, 46.7], 5);
         rfmap.setView([-22, 48.3], 5);
         mapSize =7.35;
+    } else if (mapName === "ヨーロッパ") {
+        map.setView([50,10], 3);
+        rfmap.setView([50,10], 3);
+        mapSize = 70;
     } else if (mapName === "サン・マリノ") {
         map.setView([43.94, 12.47], 12);
         rfmap.setView([44,12.5], 12);
@@ -221,6 +268,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+//==============================ここらへんguess==============================
+
 //距離からポイント計算するやつ
 function pointCul(distance) {
     let points = Math.round(Math.pow(0.99933, (((Math.max(0,distance-(Math.sqrt(mapSize)))/1000)*(mapSize)) - 12708)));
@@ -234,11 +283,13 @@ function normalizeLongitude(lng) {
 
 // 距離を計算して表示する関数
 function guess() {
-    if (noPin && countdownTime > 0) {
+    if (noPin && (countdownTime > 0 || countdownTime == undefined)) {
         return; // ピンが置かれていない場合は処理を中止
     }
 
     clearInterval(timerInterval); // タイマー停止
+    timerInterval = null;
+
     openResult();
 
     bgm.src = ambientBgm;
@@ -251,12 +302,11 @@ function guess() {
     soundEffect.play(); // 効果音を再生
 
     window.dispatchEvent(new Event('resize'));
-    if (ranswermarker) {
+    if (rline) {
         rfmap.removeLayer(rmarker);
         rfmap.removeLayer(ranswermarker);
         rfmap.removeLayer(rline);
     }
-
     if (!noPin){
         const latLng1 = L.latLng((marker.getLatLng().lat), normalizeLongitude(marker.getLatLng().lng));
         const latLng2 = L.latLng(currentLat, currentLng);
@@ -289,7 +339,10 @@ function guess() {
         } else {
             targetDistance = Math.round(distance) + "m";
         }
-    
+        
+        let totalPoint = document.getElementById('totalPoint');
+        totalPoint.textContent = parseFloat(totalPoint.textContent) + finalpoint;
+
         // 数値をアニメーションで変化させる
         animateValue('distance-value', targetDistance, 1000); // 1秒かけて変化
         animateValue('point-value', finalpoint.toString(), 1000); // finalpointを文字列に変換
@@ -299,6 +352,9 @@ function guess() {
         const latLng2 = L.latLng(currentLat, currentLng);
         ranswermarker = L.marker([latLng2.lat, latLng2.lng]).addTo(rfmap); 
     }
+    ranswermarker.on('click', function () {
+        window.open(streetViews[currentStreetViewIndex].answerUrl, '_blank');
+    });
 }
 
 // 数値をアニメーションで変化させる関数
